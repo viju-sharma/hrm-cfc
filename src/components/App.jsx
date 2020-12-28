ef81673d22ae201e172fae732773fe8d21bc8665
vijender <vijendersharma4711@gmail.com>
2020-12-28 20:01:10 +0530

    changed type of hotel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App(){
   return(
     <div>
-      <h1 className="jumbotron">Hotel Resource Management</h1>
+      <h1 className="jumbotron">Human Resource Management</h1>
     </div>
   );
 }
